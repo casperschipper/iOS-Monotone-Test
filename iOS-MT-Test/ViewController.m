a828e5fa05c5fcf4b7cbb226a47a8ed0073b71ea
Emmanuel Flores Elias <efeapple@gmail.com>
2013-01-22 15:55:14 +0100

    Added a horrible button

--- iOS-MT-Test/ViewController.m
+++ iOS-MT-Test/ViewController.m
@@ -25,12 +25,24 @@
     theLabel.text = @"hello there";
     [theLabel setBackgroundColor:[UIColor clearColor]];
     [self.view addSubview:theLabel];
+    
+    //third push
+    UIButton *theButton = [[UIButton alloc]initWithFrame:CGRectMake(100, 300, 200, 20)];
+    [theButton addTarget:self action:@selector(doSomething:) forControlEvents:UIControlEventTouchDown];
+    [theButton setBackgroundColor:[UIColor blueColor]];
+    [theButton setTitle:@"Press me" forState:UIControlStateNormal];
+    [theButton setTitleColor:[UIColor blackColor] forState:UIControlEventTouchUpInside];
+    [self.view addSubview:theButton];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)doSomething:(id)sender{
+    NSLog(@"hello git!");
 }
 
 @end
